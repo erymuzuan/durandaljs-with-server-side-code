6b43db65d4d8cceaeb686128cbb4d5a80a270541
erymuzuan <erymuzuan@hotmail.com>
2013-06-24 07:58:34 +0800

    Souce files

---
+++ web/Controllers/AppController.config.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Bespoke.Station.Web.Controllers
+{
+    public partial class AppController
+    {
+
+        public ActionResult ConfigJs()
+        {
+            return View();
+        }
+
+        [ActionName("config.js")]
+        public ActionResult Config(string id)
+        {
+            this.Response.ContentType = APPLICATION_JAVASCRIPT;
+            var script = this.RenderScript("ConfigJs");
+            return Content(script);
+        }
+
+
+    }
+}
---
+++ web/Controllers/AppController.something.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Bespoke.Station.Web.Controllers
+{
+    public partial class AppController
+    {
+
+        public ActionResult SomethingJs()
+        {
+            return View();
+        }
+
+        public ActionResult SomethingHtml()
+        {
+            return View();
+        }
+
+    }
+}
